80435f312cc2a3f3dbbadd94c7ab368372ff5a38
Flávio Tassan <tassanf@gmail.com>
2019-10-29 15:52:47 -0300

    Função para criar posições aleatórias

--- snake.py
+++ snake.py
@@ -14,6 +14,12 @@ snake_positions = [(300, 300), (310, 300), (320, 300)]
 snake = pygame.Surface((10, 10))
 snake.fill((0, 255, 0))
 
+def posicao_aleatoria():
+    x = random.randint(0, 630)
+    y = random.randint(0, 470)
+    return (x//10 * 10, y//10 * 10)
+
+
 # Definindo a nossa maçã
 apple = pygame.Surface((10, 10))
 apple.fill((255, 0, 0))
@@ -27,9 +33,7 @@ while running:
             pygame.quit()
             exit()
 
-        x = random.randint(0, 630)
-        y = random.randint(0, 470)
-        screen.blit(apple, (x//10 * 10, y//10 * 10))
+        screen.blit(apple, posicao_aleatoria())
         
         for pos in snake_positions:
             screen.blit(snake, pos)
